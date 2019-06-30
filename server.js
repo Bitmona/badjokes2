729c730a85c6b941729d2f2428484999f973088b
Bitmona <alexbrown422@gmail.com>
2019-06-30 09:37:28 -0400

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(__dirname + '/index.html');
 });
 
 // listen for requests :)
