1f09103e4f7b2492836cb15fdfa419aac29ed105
Bitmona <alexbrown422@gmail.com>
2020-05-14 09:42:44 -0400

    Search bar

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 console.log('hi');
 
+
 function myFunction() {
   var laughs = Number(prompt("How many jokes did you laugh at?"))
   if (laughs < 127) {
@@ -28,25 +29,6 @@ function myFunction() {
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
  window.location.assign("https://mobile-jokes.glitch.me/")
 }
-
-
-function search() {
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = li.textContent || li.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-}
-
 
 
 
