db575f4b69dfef090ef2820fa47f36894c61ee82
Bitmona <alexbrown422@gmail.com>
2020-06-04 13:37:23 -0400

    updates

--- client.js
+++ client.js
@@ -1,9 +1,30 @@
-console.log('hi');
+var express = require('express');
+var path = require('path');
+
+// start express module
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+/*
+ * Visit the home page
+ */
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+var server = app.listen(process.env.PORT || 3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('My site started at http://%s:%s', host, port);
+});
+
+app.get('/animals', function (req, res) {
+  res.sendFile(__dirname + '/animals.html');
+});
 
 
-function signup() {
-  console.log("ok")
-}
 
 
 function myFunction() {
@@ -29,7 +50,7 @@ function myFunction() {
   } else { 
     alert("Bad results! Try a number under 127.")
   }
-}
+} 
 
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
  window.location.assign("https://mobile-jokes.glitch.me/")
