25e452fa10986ce76be843cdc63cd942f016dd9d
Bitmona <alexbrown422@gmail.com>
2020-08-23 11:53:54 -0400

    fix

--- client.js
+++ client.js
@@ -1,28 +1,4 @@
-var express = require('express');
-var path = require('path');
 
-// start express module
-var app = express();
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-/*
- * Visit the home page
- */
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
-
-var server = app.listen(process.env.PORT || 3000, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log('My site started at http://%s:%s', host, port);
-});
-
-app.get('/animals', function (req, res) {
-  res.sendFile(__dirname + '/animals.html');
-});
 
 
 
